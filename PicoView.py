ea8e5fc3bbfee76b891eaabc19ab5fb7c21fc345
vodkazc <vodkazc@gmail.com>
2016-06-06 18:30:53 +0800

    2016.6.6.1

--- PicoView.py
+++ PicoView.py
@@ -1,14 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
-if __name__ == '_main_':
+class Example(QWidget):
 
+    def __init__(self, parent=None):
+        super(Example, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("Quit Button")
+        self.show()
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec_())
+
