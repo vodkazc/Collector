5710e1114c5e4178bd47ea1b07ab8620a50e16ba
vodkazc <vodkazc@gmail.com>
2016-06-28 17:58:14 +0800

    Complete read trace header

--- test.py
+++ test.py
@@ -1,9 +1,63 @@
 import logging
 from TraceParser import Trace
+import sys
+import os
+import matplotlib
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtGui, QtCore, QtWidgets
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
+
+class MyMplCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi= dpi)
+        self.axes = fig.add_subplot(111)
+        self.axes.hold(False)
+        self.compute_initial_figure()
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t,s)
+
+class AppliacationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("application main window")
+        self.file_menu = QtWidgets.QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit, QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+        self.main_widget = QtWidgets.Qwidget(self)
+
+        l = QtWidgets.QVBoxLayout(self.main_widget)
+        sc = MyMplCanvas(self.main_widget, width=5, height=5, dpi=100)
+        l.addWidget(sc)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("All hail matplotlib!", 2000)
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self):
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='logger.log', level=logging.DEBUG)
-    traceSample = Trace('F:\\Material\TraceSet\\riscure trace set\\AES trace simulation.trs')
+    #logging.basicConfig(filename='logger.log', level=logging.DEBUG)
+    #traceSample = Trace('F:\\Material\TraceSet\\riscure trace set\\AES trace simulation.trs')
     #x = b'\x41\x42'.hex()
 
 
